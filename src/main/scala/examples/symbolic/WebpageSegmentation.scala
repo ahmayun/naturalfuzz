74584640a0587766cceaeff4c4a4a53e5b462235
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2022-06-10 10:15:15 -0700

    Remove explicit implicit conversions

--- src/main/scala/examples/symbolic/WebpageSegmentation.scala
+++ src/main/scala/examples/symbolic/WebpageSegmentation.scala
@@ -6,7 +6,7 @@ import provenance.rdd.ProvenanceRDD.toPairRDD
 import runners.Config
 import sparkwrapper.SparkContextWithDP
 import taintedprimitives.SymImplicits._
-import taintedprimitives.{TaintedInt, TaintedString}
+import taintedprimitives.TaintedInt
 
 object WebpageSegmentation {
   def main(args: Array[String]): Unit = {
@@ -21,19 +21,19 @@ object WebpageSegmentation {
     Provenance.setProvenanceType("dual")
     val before = ctx.textFileProv(before_data, _.split(','))
     val after = ctx.textFileProv(after_data, _.split(','))
-    val boxes_before = toPairRDD[TaintedString, (TaintedString, Vector[TaintedInt])](before.map(r => (r(0)+"*"+r(r.length - 2)+"*"+r.last, (r(0), r.slice(1, r.length-2).map(_.toInt).toVector))))
-    val boxes_after = toPairRDD[TaintedString, (TaintedString, Vector[TaintedInt])](after.map(r => (r(0)+"*"+r(r.length - 2)+"*"+r.last, (r(0), r.slice(1, r.length-2).map(_.toInt).toVector))))
+    val boxes_before = before.map(r => (r(0)+"*"+r(r.length - 2)+"*"+r.last, (r(0), r.slice(1, r.length-2).map(_.toInt).toVector)))
+    val boxes_after = after.map(r => (r(0)+"*"+r(r.length - 2)+"*"+r.last, (r(0), r.slice(1, r.length-2).map(_.toInt).toVector)))
     val boxes_after_by_site_ungrouped = after.map(r => (r(0), (r.slice(1, r.length - 2).map(_.toInt).toVector, r(r.length - 2), r.last)))
     val boxes_after_by_site = _root_.monitoring.Monitors.monitorGroupByKey(boxes_after_by_site_ungrouped, 0)
 
     val pairs = _root_.monitoring.Monitors.monitorJoin(boxes_before, boxes_after, 1)
-    val changed = toPairRDD[TaintedString, (Vector[TaintedInt], TaintedString, TaintedString)](pairs.filter({
+    val changed = pairs.filter({
       case (_, ((_, v1), (_, v2))) => !v1.equals(v2)
     }).map({
       case (k, (_, (url, a))) =>
         val Array(_, cid, ctype) = k.split('*')
         (url, (a, cid, ctype))
-    }))
+    })
     val inter = _root_.monitoring.Monitors.monitorJoin(changed, boxes_after_by_site, 2)
     inter.map{
       case (url, ((box1, _, _), lst)) =>
