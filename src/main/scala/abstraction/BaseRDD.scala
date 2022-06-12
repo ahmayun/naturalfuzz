24a644bfb224ba8911ff17020a42b1c2413abe0c
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2022-06-12 07:41:53 -0700

    Add toString

--- src/main/scala/abstraction/BaseRDD.scala
+++ src/main/scala/abstraction/BaseRDD.scala
@@ -1,8 +1,13 @@
 package abstraction
 
 import scala.reflect.ClassTag
+import scala.util.Random
 
 class BaseRDD[T:ClassTag](val data: Seq[T]) extends RDD[T]{
+  def takeSample(bool: Boolean, n: Int): Array[T] = {
+    new Random().shuffle(data).take(n).toArray
+  }
+
 
   def filter(f:T=>Boolean): BaseRDD[T] = {
     new BaseRDD(data.filter(f))
@@ -18,12 +23,16 @@ class BaseRDD[T:ClassTag](val data: Seq[T]) extends RDD[T]{
 
   override def flatMap[U: ClassTag](f: T => TraversableOnce[U]): RDD[U] = new BaseRDD(data.flatMap(f))
 
-  override def take(num: Int): Array[T] = ???
+  override def take(num: Int): Array[T] = data.toArray
 
   override def setName(name: String): BaseRDD.this.type = ???
 
   override def sortBy[K](f: T => K, ascending: Boolean)
                         (implicit ord: Ordering[K], ctag: ClassTag[K]): RDD[T] =
     new BaseRDD(data.sortBy(f))
+
+  override def toString(): String = {
+    data.mkString("\n")
+  }
 }
 
