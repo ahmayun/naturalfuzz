296baaf651b94b26f4bff7f8aa350a8bbd35cfaa
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2022-06-16 21:22:05 -0700

    Refactor Config.scala

--- src/main/scala/runners/Config.scala
+++ src/main/scala/runners/Config.scala
@@ -15,7 +15,7 @@ object Config {
   val deepFaults = false
   val seedType = "weak" //either full, reduced or weak
   val benchmarkClass = s"examples.${if (faultTest) "faulty" else "fuzzable"}.$benchmarkName"
-  val mutateProbs = Array( // 0:M1, 1:M2 ... 5:M6
+  val mutateProbs: Array[Float] = Array( // 0:M1, 1:M2 ... 5:M6
     0.9f, // Data
     0.02f, // Data
     0.02f, // Format
@@ -24,7 +24,7 @@ object Config {
     0.02f) // Format
 
 
-  val mapInputFilesWeak = Map(
+  val mapInputFilesWeak: Map[String, Array[String]] = Map(
     "FlightDistance" -> Array("seeds/weak_seed/FlightDistance/flights", "seeds/weak_seed/FlightDistance/airports_data"),
     "WebpageSegmentation" -> Array("seeds/weak_seed/webpage_segmentation/before", "seeds/weak_seed/webpage_segmentation/after"),
     "CommuteType" -> Array("seeds/weak_seed/commute/trips"),
@@ -33,7 +33,7 @@ object Config {
     "DeliveryFaults" -> Array("seeds/weak_seed/deliveryfaults/deliveries")
   )
 
-  val mapInputFilesFull = Map(
+  val mapInputFilesFull: Map[String, Array[String]] = Map(
     "WebpageSegmentation" -> Array("seeds/full_data/webpage_segmentation/before", "seeds/full_data/webpage_segmentation/after"),
     "CommuteType" -> Array("seeds/full_data/trips"),
     "Customers" -> Array("seeds/full_data/customers/customers", "seeds/full_data/customers/orders"),
@@ -42,7 +42,7 @@ object Config {
     "Delays" -> Array("seeds/full_data/delays/station1", "seeds/full_data/delays/station2")
   )
 
-  val mapInputFilesReduced = Map(
+  val mapInputFilesReduced: Map[String, Array[String]] = Map(
     "WebpageSegmentation" -> Array("seeds/reduceddata/webpage_segmentation/before", "seeds/reduceddata/webpage_segmentation/after"),
     "CommuteType" -> Array("seeds/reduceddata/trips"),
     "Customers" -> Array("seeds/reduceddata/customers/customers", "seeds/reduceddata/customers/orders"),
@@ -62,7 +62,7 @@ object Config {
     if (switch) faulty else normal
   }
 
-  val mapFunFuzzables = Map[String, Array[String] => Unit](elems =
+  val mapFunFuzzables: Map[String, Array[String] => Unit] = Map[String, Array[String] => Unit](elems =
     "FlightDistance" -> Switch(fuzzable.FlightDistance.main, faulty.FlightDistance.main, faultTest),
     "WebpageSegmentation" -> Switch(fuzzable.WebpageSegmentation.main, faulty.WebpageSegmentation.main, faultTest),
     "CommuteType" -> Switch(fuzzable.CommuteType.main, faulty.CommuteType.main, faultTest),
@@ -71,7 +71,7 @@ object Config {
     "DeliveryFaults" -> Switch(fuzzable.DeliveryFaults.main, faulty.DeliveryFaults.main, faultTest)
   )
 
-  val mapFunSpark = Map[String, Array[String] => Unit](elems =
+  val mapFunSpark: Map[String, Array[String] => Unit] = Map[String, Array[String] => Unit](elems =
     "FlightDistance" -> benchmarks.FlightDistance.main,
     "WebpageSegmentation" -> benchmarks.WebpageSegmentation.main,
     "CommuteType" -> benchmarks.CommuteType.main,
@@ -80,7 +80,7 @@ object Config {
     "DeliveryFaults" -> benchmarks.DeliveryFaults.main
   )
 
-  val mapFunProbeAble = Map[String, Array[String] => ProvInfo](elems =
+  val mapFunProbeAble: Map[String, Array[String] => ProvInfo] = Map[String, Array[String] => ProvInfo](elems =
     "FlightDistance" -> monitored.FlightDistance.main,
     "WebpageSegmentation" -> monitored.WebpageSegmentation.main,
     "CommuteType" -> monitored.CommuteType.main,
@@ -89,7 +89,7 @@ object Config {
     "DeliveryFaults" -> monitored.DeliveryFaults.main
   )
 
-  val mapSchemas = Map[String, Array[Array[Schema[Any]]]](elems =
+  val mapSchemas: Map[String, Array[Array[Schema[Any]]]] = Map[String, Array[Array[Schema[Any]]]](elems =
     "FlightDistance" -> BenchmarkSchemas.SYNTHETIC3,
     "WebpageSegmentation" -> BenchmarkSchemas.SEGMENTATION,
     "CommuteType" -> BenchmarkSchemas.COMMUTE,
@@ -98,7 +98,7 @@ object Config {
     "DeliveryFaults" -> BenchmarkSchemas.FAULTS
   )
 
-  val mapErrorCountAll = Map[String, Int](elems =
+  val mapErrorCountAll: Map[String, Int] = Map[String, Int](elems =
     "FlightDistance" -> 7,
     "WebpageSegmentation" -> 10,
     "CommuteType" -> 6,
@@ -107,7 +107,7 @@ object Config {
     "DeliveryFaults" -> 7
   )
 
-  val mapErrorCountDeep = Map[String, Int](elems =
+  val mapErrorCountDeep: Map[String, Int] = Map[String, Int](elems =
     "FlightDistance" -> 3,
     "WebpageSegmentation" -> 5,
     "CommuteType" -> 3,
