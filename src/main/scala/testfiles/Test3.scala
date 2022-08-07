7b7c6fc9dcdca434b0fd7ebaff634f41afe7d8ca
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2022-08-07 15:12:52 -0400

    Sync

--- src/main/scala/testfiles/Test3.scala
+++ src/main/scala/testfiles/Test3.scala
@@ -9,6 +9,7 @@ import scala.collection.mutable.ListBuffer
 object Test3 {
 
   def main(args: Array[String]): Unit = {
+    println("opening file and never closing")
     new FileWriter(new File("randomtest"))
   }
 }
