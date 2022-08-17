80952f8f33979fc8b528dbc836c1126cbc8d7188
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2022-08-17 07:28:07 -0700

    debug

--- src/main/scala/examples/clustermonitor/WordCount.scala
+++ src/main/scala/examples/clustermonitor/WordCount.scala
@@ -11,8 +11,8 @@ object WordCount extends Serializable {
   def main(args: Array[String]): ProvInfo = {
     println(s"WordCount args ${args.mkString(",")}")
     val sparkConf = new SparkConf()
-    if (args.length < 3) throw new IllegalArgumentException("Program was called with too few args")
-    sparkConf.setMaster(args(2))
+    if (args.length < 2) throw new IllegalArgumentException("Program was called with too few args")
+    sparkConf.setMaster(args(1))
     sparkConf.setAppName("Prov WordCount")
     val ctx = new SparkContextWithDP(new SparkContext(sparkConf))
     ctx.setLogLevel("ERROR")
