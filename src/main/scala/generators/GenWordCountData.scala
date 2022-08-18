bf67b0490f7583abd26e7c37e11ec19d6c6ada64
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2022-08-18 11:38:37 -0700

    debug

--- src/main/scala/generators/GenWordCountData.scala
+++ src/main/scala/generators/GenWordCountData.scala
@@ -5,48 +5,35 @@ import scala.util.Random
 object GenWordCountData extends Serializable {
 
 
-  def generateURL(len: Int): String = {
-    s"www.${Random.alphanumeric.take(len).mkString}.com"
-  }
+  def main(args: Array[String]): Unit = {
 
-  def getRandomComponentType: String = {
-    val types = Array(
-      "advertisement",
-      "header",
-      "footer",
-      "heading",
-      "logo"
+    val partitions = args(0).toInt // e.g. 200
+    val dataper = args(1).toInt // e.g. 100000
+    val name = args(2)
+    val seed = Random.nextLong()
+    Random.setSeed(seed)
+
+    val sparkConf = new SparkConf()
+    val datasets = Array(
+      ("ds1", s"hdfs://zion-headnode:9000/ahmad/$name")
     )
-    types(Random.nextInt(types.length))
-  }
+    sparkConf.setMaster("spark://zion-headnode:7077")
+    sparkConf.setAppName("DataGen: WordCounts")
 
-  def main(args:Array[String]): Unit = {
-
-      val partitions = args(0).toInt // e.g. 200
-      val dataper  = args(1).toInt // e.g. 100000
-      val seed = Random.nextLong()
-      Random.setSeed(seed)
-
-      val sparkConf = new SparkConf()
-      val datasets = Array(
-        ("ds1", "hdfs://zion-headnode:9000/ahmad/WordCount")
-      )
-      sparkConf.setMaster("spark://zion-headnode:7077")
-      sparkConf.setAppName("DataGen: WordCounts")
-
-      println(
+    println(
       s"""
          |partitions: $partitions
          |records: $dataper
          |seed: $seed
          |""".stripMargin
-      )
+    )
 
     datasets.foreach { case (_, f) =>
-      SparkContext.getOrCreate(sparkConf).parallelize(Seq[Int]() , partitions).mapPartitions { _ =>
-        (1 to dataper).map{_ =>
+      SparkContext.getOrCreate(sparkConf).parallelize(Seq[Int](), partitions).mapPartitions { _ =>
+        (1 to dataper).map { _ =>
           s"This is a sentence"
-        }.iterator}.saveAsTextFile(f)
+        }.iterator
+      }.saveAsTextFile(f)
     }
   }
 
