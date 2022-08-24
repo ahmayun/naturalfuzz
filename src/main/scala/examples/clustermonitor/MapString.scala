1c04e54b09d460bfcbb1134e3033e58be1739c83
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2022-08-24 00:10:30 -0700

    fix

--- src/main/scala/examples/clustermonitor/MapString.scala
+++ src/main/scala/examples/clustermonitor/MapString.scala
@@ -10,7 +10,7 @@ object MapString extends Serializable {
     val conf = new SparkConf()
     if (args.length < 2) throw new IllegalArgumentException("Program was called with too few args")
     conf.setMaster(args(1))
-    conf.setAppName("WordCount")
+    conf.setAppName("MapString")
 
     val sc = new SparkContextWithDP(new SparkContext(conf))
     sc.textFileProv(args(0), _.split("\n")).map { s =>
