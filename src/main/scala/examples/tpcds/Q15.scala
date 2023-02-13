27e39e218a1862cf3c1d781c81c4bac049b29344
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2023-02-13 07:23:11 -0800

    Q15 added

--- src/main/scala/examples/tpcds/Q15.scala
+++ src/main/scala/examples/tpcds/Q15.scala
@@ -4,7 +4,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.util.Random
 
-object Q2 extends Serializable {
+object Q15 extends Serializable {
 
   def main(args: Array[String]) {
     val conf = new SparkConf()
@@ -16,11 +16,54 @@ object Q2 extends Serializable {
     val seed = "ahmad".hashCode()
     val rand = new Random(seed)
     val YEAR = rand.nextInt(2002 - 1998) + 1998
+    val QOY = rand.nextInt(1) + 1
+    val ZIPS = List("85669","86197","88274","83405","86475","85392","85460","80348","81792")
+    val STATES = List("CA", "WA", "GA")
 
-    val web_sales = sc.textFile(s"$datasetsPath/web_sales").map(_.split(","))
-    val date_dim = sc.textFile(s"$datasetsPath/date_dim").map(_.split(","))
     val catalog_sales = sc.textFile(s"$datasetsPath/catalog_sales").map(_.split(","))
     val customer = sc.textFile(s"$datasetsPath/customer").map(_.split(","))
+    val customer_address = sc.textFile(s"$datasetsPath/customer_address").map(_.split(","))
+    val date_dim = sc.textFile(s"$datasetsPath/date_dim").map(_.split(","))
+
+    val filtered_dd = date_dim
+      .filter {
+        row =>
+          val d_qoy = row(10)
+          val d_year = row(6)
+          d_qoy == QOY.toString && d_year == YEAR.toString
+      }
+
+    catalog_sales
+      .map(row => (row(2)/*cs_bill_customer_sk*/, row))
+      .join(customer.map(row => (row.head, row)))
+      .map {
+        case (_, (cs_row, c_row)) =>
+          (c_row(4)/*c_current_addr_sk*/, (cs_row, c_row))
+      }
+      .join(customer_address.map(row => (row.head, row)))
+      .map {
+        case (_, ((cs_row, c_row), ca_row)) =>
+          (cs_row.last/*cs_sold_date_sk*/, (cs_row, c_row, ca_row))
+      }
+      .filter {
+        case (_, (cs_row, c_row, ca_row)) =>
+          val ca_zip = try { ca_row(9) } catch { case _ => "error"} // took liberty here (if the row is malformed for some reason
+          val ca_state = try { ca_row(8) } catch { case _ => "error"}
+          val cs_sales_price = convertColToFloat(cs_row, 20)
+
+          ca_zip != "error" && ca_state != "error" &&
+            (ZIPS.contains(ca_zip.take(5)) || cs_sales_price > 500 || STATES.contains(ca_state))
+      }
+      .join(filtered_dd.map(row => (row.head, row)))
+      .map {
+        case (_, ((cs_row, c_row, ca_row), dd_row)) =>
+          val cs_sales_price = convertColToFloat(cs_row, 20)
+          (ca_row(9)/*ca_zip*/, cs_sales_price)
+      }
+      .reduceByKey(_+_)
+      .sortBy(_._1)
+      .take(10)
+      .foreach(println)
 
     
   }
