ac267c6468b1ef3efe049432c61d43d0d78e9cb2
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2023-02-17 13:03:42 -0500

    fixed usage comment

--- run-provfuzz.sh
+++ run-provfuzz.sh
@@ -9,7 +9,7 @@
 # . Process the measurement files produced using CoverageMeasurementConsolidator.scala
 
 # SAMPLE RUN:
-#       ./run-bigfuzz.sh DeliveryFaults faulty 86400; echo "Subject: $(hostname): bigfuzz (exit $?)" | sendmail ahmad35@vt.edu
+#       ./run-provfuzz.sh DeliveryFaults faulty 86400 full; echo "Subject: $(hostname): cofuzz (exit $?)" | sendmail ahmad35@vt.edu
 
 # Temporarily hard-coded, should be parsed from args
 NAME=$1
