9b897edae6dd5b537040294c86e8869524ed5b68
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2023-03-25 12:25:16 -0700

    add accumulator to rrfjc

--- src/main/scala/runners/RunRIGFuzzJarCluster.scala
+++ src/main/scala/runners/RunRIGFuzzJarCluster.scala
@@ -24,6 +24,34 @@ object RunRIGFuzzJarCluster extends Serializable {
     println("RunRIGFuzzJar called with following args:")
     println(args.mkString("\n"))
 
+    class ExpressionAccumulatorParam extends AccumulatorV2[SymbolicExpression, List[SymbolicExpression]] {
+      private var expressionList: List[SymbolicExpression] = List()
+
+      def isZero: Boolean = expressionList.isEmpty
+
+      def copy(): AccumulatorV2[SymbolicExpression, List[SymbolicExpression]] = {
+        val newAcc = new ExpressionAccumulatorParam
+        newAcc.expressionList = this.expressionList
+        newAcc
+      }
+
+      def reset(): Unit = {
+        expressionList = List()
+      }
+
+      def add(v: SymbolicExpression): Unit = {
+        expressionList = expressionList :+ v
+      }
+
+      def merge(other: AccumulatorV2[SymbolicExpression, List[SymbolicExpression]]): Unit = other match {
+        case acc: ExpressionAccumulatorParam => expressionList = acc.expressionList ::: expressionList
+        case _ => throw new UnsupportedOperationException(
+          s"Cannot merge ${this.getClass.getName} with ${other.getClass.getName}")
+      }
+
+      def value: List[SymbolicExpression] = expressionList
+    }
+
     // ==P.U.T. dependent configurations=======================
     val (benchmarkName, sparkMaster, pargs, duration, outDir) =
     if(!args.isEmpty) {
@@ -71,6 +99,11 @@ object RunRIGFuzzJarCluster extends Serializable {
         .setAppName(s"RunRIGFuzzJar: symbolic.${benchmarkName}")
     )
     sc.setLogLevel("ERROR")
+
+    // create an accumulator in the driver and initialize it to an empty list
+    val expressionAccumulator = new ExpressionAccumulatorParam
+    sc.register(expressionAccumulator, "ExpressionAccumulator")
+    monitoring.Monitors.setAccumulator(expressionAccumulator)
 
     // Preprocessing and Fuzzing
     println("Running monitored program")
