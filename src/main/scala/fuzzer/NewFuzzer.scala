89d7e412f5d1cde579fbb214c03e790cfe77409f
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2023-04-14 13:20:37 -0700

    offset by one

--- src/main/scala/fuzzer/NewFuzzer.scala
+++ src/main/scala/fuzzer/NewFuzzer.scala
@@ -51,7 +51,7 @@ object NewFuzzer {
   }
 
   def getElapsedSeconds(t_start: Long): Float = {
-    (System.currentTimeMillis() - t_start)/1000.0f
+    (System.currentTimeMillis() - t_start)/1000.0f + 1
   }
 
   def FuzzMutants(refProgram: ExecutableProgram, mutantProgram: ExecutableProgram, guidance: Guidance, outDir: String, compile: Boolean = true): (FuzzStats, Long, Long) = {
