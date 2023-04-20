570f3d3bf606de5e1daa81b5ef02b421f7bf29dd
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2023-04-19 19:29:59 -0700

    Q3 motivating prints

--- src/main/scala/examples/symbolic/Q3.scala
+++ src/main/scala/examples/symbolic/Q3.scala
@@ -57,7 +57,7 @@ object Q3 extends Serializable {
       // and store_sales.ss_item_sk = item.i_item_sk
 
     val safety2 = item.filter{ row => try { row(13).toInt; true } catch { case _: Throwable => false}}
-    val filter2 = saftey2.filter(row => _root_.monitoring.Monitors.monitorPredicateSymEx(row(13).toInt /*i_manufact_id*/ == MANUFACT, (List(), List()), 1, expressionAccumulator)) // and item.i_manufact_id = [MANUFACT]
+    val filter2 = safety2.filter(row => _root_.monitoring.Monitors.monitorPredicateSymEx(row(13).toInt /*i_manufact_id*/ == MANUFACT, (List(), List()), 1, expressionAccumulator)) // and item.i_manufact_id = [MANUFACT]
     val map4 = filter2.map(row => (row.head, row))
 
     println("filter2.map4")
