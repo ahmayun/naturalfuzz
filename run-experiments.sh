4280cde2b967260da0496f4b55b3ef8e179cba8e
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2023-04-22 11:22:50 -0700

    sync

--- run-experiments.sh
+++ run-experiments.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
-fuzz-mutants() {
+
+fuzz-mutant() {
   QUERY=$1
+  MUTANT=$2
+  echo "starting fuzzing for mutant $MUTANT of program $QUERY"
+}
+
+run-fuzzing() {
+  QUERY=$1
+  echo "starting fuzzing tests for $QUERY"
   DIR="src/main/scala/examples/mutants/$QUERY"
   for mutant in "$DIR"/Q*; do
-    echo "MUTANT $mutant"
+    MUTANT=$(drop-path-and-ext $mutant)
+    fuzz-mutant $QUERY $MUTANT
   done
+  unset QUERY
+  unset MUTANT
+  unset DIR
+}
+
+drop-path-and-ext() {
+  file=$1
+  FILE=$(basename "$file") # drop path prefix
+  echo ${FILE%.*} # drop .scala extension
+}
+
+run-overhead-test() {
+  QUERY=$1
+  echo "starting overhead test for $QUERY"
+#  echo "./cluster-assemble-and-distribute.sh runners.RunRIGFuzzOverheadTest Q1 spark://zion-headnode:7077 ~/overheadtests /TPCDS_1G_NOHEADER_NOCOMMAS/{store_returns,date_dim,store,customer}"
+  unset QUERY
 }
 
 
 # Loop from 1 to 10
 for i in $(seq 1 10); do
     for file in src/main/scala/examples/faulty/Q*; do
-      SCALAFILE=$(basename "$file") # drop path prefix
-      PROGRAM=${SCALAFILE%.*} # drop .scala extension
-      echo "Running EXPERIMENT $i for $PROGRAM"
-      fuzz-mutants $PROGRAM
+      PROGRAM=$(drop-path-and-ext $file)
+      echo "running EXPERIMENT $i for $PROGRAM"
+      run-overhead-test $PROGRAM
+      run-fuzzing-test $PROGRAM
     done
 done
