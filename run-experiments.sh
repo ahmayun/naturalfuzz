1b462937246cd34d67b9f23c5143b4517c2bb526
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2023-04-22 10:55:13 -0700

    sync

--- run-experiments.sh
+++ run-experiments.sh
@@ -12,7 +12,7 @@ fuzz-mutants() {
 for i in $(seq 1 10); do
   # Command to be executed in the loop
   echo "Loop iteration: $i"
-  ./cluster-assemble-and-distribute.sh runners.RunRIGFuzzOverheadTest Q1 spark://zion-headnode:7077 ~/rig-experiments-test /TPCDS_1G_NOHEADER_NOCOMMAS/{store_returns,date_dim,store,customer}
+#  ./cluster-assemble-and-distribute.sh runners.RunRIGFuzzOverheadTest Q1 spark://zion-headnode:7077 ~/rig-experiments-test /TPCDS_1G_NOHEADER_NOCOMMAS/{store_returns,date_dim,store,customer}
   fuzz-mutants Q1
 #  ./cluster-assemble-and-distribute.sh runners.RunRIGFuzzOverheadTest Q3 spark://zion-headnode:7077 ~/overheadtests /TPCDS_1G_NOHEADER_NOCOMMAS/{store_sales,date_dim,item}
 #  ./cluster-assemble-and-distribute.sh runners.RunRIGFuzzOverheadTest Q6 spark://zion-headnode:7077 ~/overheadtests /TPCDS_1G_NOHEADER_NOCOMMAS/{customer_address,customer,store_sales,date_dim,item}
