e484bc24a44034ea1201bad4b95865ea943db4bc
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2023-04-22 10:51:29 -0700

    fix path

--- run-experiments.sh
+++ run-experiments.sh
@@ -5,7 +5,7 @@ for i in $(seq 1 10); do
   # Command to be executed in the loop
   echo "Loop iteration: $i"
 #  ./cluster-assemble-and-distribute.sh runners.RunRIGFuzzOverheadTest Q1 spark://zion-headnode:7077 ~/rig-experiments-test /TPCDS_1G_NOHEADER_NOCOMMAS/{store_returns,date_dim,store,customer}
-  DIR="examples/mutants/Q1"
+  DIR="src/main/scala/examples/mutants/Q1"
   # Loop through all files ending with ".info"
   for file in "$DIR"/Q*; do
     echo "Processing file $file"
