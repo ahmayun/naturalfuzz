5afd6cbcca1cfb88c0471799c97cc2b24ff93ac7
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2023-04-23 08:20:16 -0700

    sync

--- run-fuzzing-experiments.sh
+++ run-fuzzing-experiments.sh
@@ -4,6 +4,47 @@
 #     ./run-experiments.sh experimentsdir:~/final-experiments datasetsdir:src/seeds/bigfuzz
 
 EXPERIMENTS_DIR=$1
+DIR_BIGFUZZ_SAMPLES=$2
+
+get-dataset-paths() {
+  QUERY=$1
+
+  case $QUERY in
+    Q1)
+      eval echo "$DIR_BIGFUZZ_SAMPLES/{store_returns,date_dim,store,customer}"
+      ;;
+    Q3)
+      eval echo "$DIR_BIGFUZZ_SAMPLES/{store_sales,date_dim,item}"
+      ;;
+    Q6)
+      eval echo "$DIR_BIGFUZZ_SAMPLES/{customer_address,customer,store_sales,date_dim,item}"
+      ;;
+    Q7)
+      eval echo "$DIR_BIGFUZZ_SAMPLES/{customer_demographics,promotion,store_sales,date_dim,item}"
+      ;;
+    Q12)
+      eval echo "$DIR_BIGFUZZ_SAMPLES/{web_sales,date_dim,item}"
+      ;;
+    Q15)
+      eval echo "$DIR_BIGFUZZ_SAMPLES/{catalog_sales,customer,customer_address,date_dim}"
+      ;;
+    Q19)
+      eval echo "$DIR_BIGFUZZ_SAMPLES/{date_dim,store_sales,item,customer,customer_address,store}"
+      ;;
+    Q20)
+      eval echo "$DIR_BIGFUZZ_SAMPLES/{catalog_sales,date_dim,item}"
+      ;;
+    *)
+      echo "Invalid query"
+      ;;
+  esac
+}
+
+drop-path-and-ext() {
+  file=$1
+  FILE=$(basename "$file") # drop path prefix
+  echo ${FILE%.*} # drop .scala extension
+}
 
 fuzz-mutant() {
   QUERY=$1
@@ -11,7 +52,9 @@ fuzz-mutant() {
   FUZZ_DIR=$3
   echo "starting fuzzing for mutant $MUTANT of program $QUERY"
   echo "RIGFUZZ"
-  echo $QUERY $MUTANT faulty 20 $FUZZ_DIR/{qrs.pkl,reduced_data/dataset_*}
+#  echo ./run-rigfuzz.sh $QUERY $MUTANT faulty 20 $FUZZ_DIR/{qrs.pkl,reduced_data/dataset_*}
+  echo "BIGFUZZ"
+  echo ./run-bigfuzz.sh $QUERY $MUTANT faulty 20 $(get-dataset-paths $QUERY)
 
 }
 
@@ -27,54 +70,6 @@ run-fuzzing-test() {
   unset QUERY
   unset MUTANT
   unset DIR
-}
-
-drop-path-and-ext() {
-  file=$1
-  FILE=$(basename "$file") # drop path prefix
-  echo ${FILE%.*} # drop .scala extension
-}
-
-get-dataset-paths() {
-  QUERY=$1
-
-  case $QUERY in
-    Q1)
-      eval echo "/TPCDS_1G_NOHEADER_NOCOMMAS/{store_returns,date_dim,store,customer}"
-      ;;
-    Q3)
-      eval echo "/TPCDS_1G_NOHEADER_NOCOMMAS/{store_sales,date_dim,item}"
-      ;;
-    Q6)
-      eval echo "/TPCDS_1G_NOHEADER_NOCOMMAS/{customer_address,customer,store_sales,date_dim,item}"
-      ;;
-    Q7)
-      eval echo "/TPCDS_1G_NOHEADER_NOCOMMAS/{customer_demographics,promotion,store_sales,date_dim,item}"
-      ;;
-    Q12)
-      eval echo "/TPCDS_1G_NOHEADER_NOCOMMAS/{web_sales,date_dim,item}"
-      ;;
-    Q15)
-      eval echo "/TPCDS_1G_NOHEADER_NOCOMMAS/{catalog_sales,customer,customer_address,date_dim}"
-      ;;
-    Q19)
-      eval echo "/TPCDS_1G_NOHEADER_NOCOMMAS/{date_dim,store_sales,item,customer,customer_address,store}"
-      ;;
-    Q20)
-      eval echo "/TPCDS_1G_NOHEADER_NOCOMMAS/{catalog_sales,date_dim,item}"
-      ;;
-    *)
-      echo "Invalid query"
-      ;;
-  esac
-}
-
-run-overhead-test() {
-  QUERY=$1
-  echo "starting overhead test for $QUERY"
-  sed -i "s/var benchmarkName = \".*\"/var benchmarkName = \"$QUERY\"/" src/main/scala/runners/Config.scala
-  ./cluster-assemble-and-distribute.sh runners.RunRIGFuzzOverheadTest $QUERY spark://zion-headnode:7077 $EXPERIMENT_DIR $(get-dataset-paths $QUERY) || exit 1
-  unset QUERY
 }
 
 
