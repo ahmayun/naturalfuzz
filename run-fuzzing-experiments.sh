b34ab8183398bb80e82152abc51623e0a04b28cc
Ahmad Humayun <ahmadhumayun3.5@gmail.com>
2023-04-23 08:55:05 -0700

    sync

--- run-fuzzing-experiments.sh
+++ run-fuzzing-experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # sample run:
-#     ./run-experiments.sh experimentsdir:~/final-experiments datasetsdir:src/seeds/bigfuzz
+#     ./run-fuzzing-experiments.sh ~/final-overhead-tests seeds/bigfuzz jazzer 20
 
 EXPERIMENTS_DIR=$1
 DIR_BIGFUZZ_SAMPLES=$2
